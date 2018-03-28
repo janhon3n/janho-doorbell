cf78b5e01ccef3a030d307cdb674000daee64fd7
Mikael Janhonen <janhon3n@gmail.com>
2018-03-28 09:56:37 +0000

    Change permissions or something

--- start-stream.sh
+++ start-stream.sh
@@ -3,4 +3,4 @@ ffmpeg \
 		-framerate 25 -video_size 640x480 -i /dev/video0 \
 	-f mpegts \
 		-codec:v mpeg1video -s 640x480 -b:v 1000k -bf 0 \
-	http://localhost:8081/doorbell
+	http://127.0.0.1:8081/doorbell
